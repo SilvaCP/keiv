89723a66bf3bba2ba929ec638aec00119d8b8a88
Carlos Pereira <silvaotl@outlook.com>
2025-10-17 16:51:47 -0300

    Adicionei as linhas 4 e 5.

--- euAprendo.py
+++ euAprendo.py
@@ -1,3 +1,5 @@
 print("É claro que eu aprendo!")
 print("FIZ ESSA MODIFICAÇÃO NO TRABALHO. ESTAVA REMOTO!")
 print("Estou acrescentando esta nova linha no código.")
+print("A soma de 6 + 6 é igual:",6 + 6) # Teste de mais uma linha acrecentada.
+print("Q")
