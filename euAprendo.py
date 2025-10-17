292d04eddcf231a855a13835b3cb2ccdcba12489
Carlos <79268743+SilvaCP@users.noreply.github.com>
2025-10-17 16:56:31 -0300

    Add a new print statement to euAprendo.py
    Adicionei  código à linha 6.

--- euAprendo.py
+++ euAprendo.py
@@ -3,3 +3,4 @@ print("FIZ ESSA MODIFICAÇÃO NO TRABALHO. ESTAVA REMOTO!")
 print("Estou acrescentando esta nova linha no código.")
 print("A soma de 6 + 6 é igual:",6 + 6) # Teste de mais uma linha acrecentada.
 print("Q")
+print("A")
